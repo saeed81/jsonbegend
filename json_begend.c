c0dab151252de60e9abd2d4d3c78b8d83d414a8b
Saeed Falahat <saeed.falahat@smhi.se>
2018-08-13 11:29:00 +0200

    stoi_bg is added

--- json_begend.c
+++ json_begend.c
@@ -241,6 +241,47 @@ int stoi(char *st){
   return (c*d);
 }
 
+int stoi_bg(char *beg, char *end){
+  int sign = 0;
+  int nc=0;
+  char *temp = beg;
+  
+  while (temp<=end ){
+    if ( *temp != ' ') nc++;
+    temp++;
+  }
+  nc--;
+  int d = 0;
+  int c = 1;
+  char *st = beg;
+  while (st<=end ){
+    if (*st == '-'){
+      c = -1;
+      nc--; 
+      st++;
+      sign += 1;
+      if (sign > 1 ) return 0;
+    }
+    else if (*st == '+'){
+      c = 1;
+      nc--; 
+      st++;
+      sign += 1;
+      if (sign > 1 ) return 0;
+    }
+    else if( *st != ' ') {
+      d += ((int)(*st - '0')*pow10(nc));
+      st++;
+      nc--;
+    }
+    else {
+      st++;
+    }
+  }
+
+  return (c*d);
+}
+
 float stof(char *st){
   
   int sign = 0;
